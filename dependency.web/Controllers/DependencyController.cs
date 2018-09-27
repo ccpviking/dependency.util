e7bac656a2852f2b19646f293531ca536991d2d8
ccpviking <ccpviking@gmail.com>
2018-09-26 22:03:00 -0600

    Update DependencyController to parse dependencies.

---
+++ dependency.web/Controllers/DependencyController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dependency.lib;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dependency.web.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DependencyController : ControllerBase
+	{
+		// valid: https://localhost:5001/api/dependency/["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: " ]
+		// valid: https://localhost:5001/api/dependency/["zero: one","one: two","two: three","three: ","four: five","five: six","six: "]
+		// invalid: https://localhost:5001/api/dependency/["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: ", "Ice: Leetmeme" ]
+		// invalid: https://localhost:5001/api/dependency/["zero: one","one: two","two: three","three: zero","four: five","five: six","six: "]
+
+		// GET api/dependency/["zero: one","one: two","two: three","three: ","four: five","five: six","six: "]
+		[HttpGet("{dependencyText}")]
+		public ActionResult<string> Get(string dependencyText)
+		{
+			var result = string.Empty;
+			try
+			{
+				var parser = new DependencyParser(new InputParser());
+				result = parser.ParseDependencies(dependencyText);
+			}
+			catch (System.Exception ex)
+			{
+				result = ex.Message;
+			}
+			return result;
+		}
+	}
+}
